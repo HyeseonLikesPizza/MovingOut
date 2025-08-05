599ff44035c10a11daca40cb109d92b7afc09a70
jammim0209 <woals1375@naver.com>
2025-08-05 15:59:30 +0900

    creat_EnemyChar
    에너미 캐릭터 생성

---
+++ Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/MovingOutCharacter.h"
+#include "EnemyMovingOutCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVINGOUT_API AEnemyMovingOutCharacter : public AMovingOutCharacter
+{
+	GENERATED_BODY()
+	
+};
