18cedb26fa36ba8aab7b928b7cfdfd7169fe6bef
Lauren <b1327e@naver.com>
2025-08-05 15:28:04 +0900

    Initial commit

---
+++ Source/MovingOut/Private/GameMode/MovingOutGameMode.cpp
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameMode/MovingOutGameMode.h"
+#include "Controller/MovingOutPlayerController.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMovingOutGameMode::AMovingOutGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AMovingOutPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
