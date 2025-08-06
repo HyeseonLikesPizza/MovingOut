c5dfad87e234aa35f4d7cf1fb187ee0f9caa0e02
jammim0209 <woals1375@naver.com>
2025-08-06 14:14:22 +0900

    캡슐 컴포넌트 콜리전 인신 및 회전
    캡슐 컨포넌트가 부딪히는 actor 의 정보를 알고 물체가 캡슐 컴포넌트에 OnHit 될떄마다 rotation 90 으로 돈다

--- Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
+++ Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
@@ -2,24 +2,35 @@
 
 #pragma once
 
+
 #include "CoreMinimal.h"
 #include "Character/MovingOutCharacter.h"
 #include "EnemyMovingOutCharacter.generated.h"
+
 
 UCLASS()
 
 class MOVINGOUT_API AEnemyMovingOutCharacter : public AMovingOutCharacter
 {
 	GENERATED_BODY()
-	
+
+
+	// 틱을 오버드라이브 함
 public:
 	AEnemyMovingOutCharacter();
 	void Tick(float DeltaSeconds) override;
 
-
+	// 비긴플레이 를 오버드라이브 일부만 하용하게
+protected:
+	virtual void BeginPlay() override;
+	
 private:
 
+	// 에너미에 힛 포인트를 불러온다
+	UFUNCTION()
+	void OnEnemyHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	// arrow 불러오기
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UArrowComponent* Arrow;
-}
-;
+};
