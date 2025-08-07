0e9d0a7db5bd32db6737e1b391dc6fe8e28eebf8
jammim0209 <woals1375@naver.com>
2025-08-07 15:40:46 +0900

    enemy 회전 부드럽게,물체에 가로막힐때 z 값 기준 좌 우 회전

--- Source/MovingOut/Private/Character/EnemyMovingOutCharacter.cpp
+++ Source/MovingOut/Private/Character/EnemyMovingOutCharacter.cpp
@@ -11,9 +11,9 @@
 #include "Engine/World.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "../DebugHelpers.h"
 
-
-
+ 
 
 // 캡술컴포넌트에 닿았을때 인지 없다면 에러뜨고 없다고 뜨기
 void AEnemyMovingOutCharacter::BeginPlay()
@@ -33,65 +33,70 @@ void AEnemyMovingOutCharacter::BeginPlay()
 	}
 }
 
-// 에너미 캐릭터가 hit 돼고 로고 text 띄어지게 만들어줌
 void AEnemyMovingOutCharacter::OnEnemyHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
+	// 충돌 로그 찍기
 	UE_LOG(LogTemp, Warning, TEXT("Enemy가 %s와 충돌했습니다!"), *OtherActor->GetName());
-	// 현재 Rotation 가져오기 n'도로 회전
-	FRotator CurrentRotation = GetActorRotation();
-	CurrentRotation.Yaw -= 45.f;
-	SetActorRotation(CurrentRotation);
-	 // 이후 y 축 방향으로 튼다
-	 // FVector CurrentDirection = GetActorLocation();
-	 // FVector Direction = FVector(0.0f, 1.0f, 0.0f);
-	 // AddMovementInput(Direction, 1.0f);
-	 // SetActorLocation(CurrentDirection);
 
-// 에너미 캐릭터가 hit 돼고 로고 text 띄어지게 만들어줌
-
-	void AEnemyMovingOutCharacter::OnEnemyHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+	//이미 Hit 처리 중이면 리턴
+	if (Bhashit)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Enemy가 %s와 충돌했습니다!"), *OtherActor->GetName());
-	
-		// boolean 불러옴
-		if (Bhashit)
-		{
-			return;
-		
-		}
-		if (OtherActor && OtherActor != this)
-		{
-			
-
-			// 현재 Rotation 가져오기 n'도로 회전
-			FRotator TargetRotation = GetActorRotation();
-			TargetRotation.Yaw -= 75.f;
-			FRotator CurrentRotation = GetActorRotation();
-			float DeltaTime = GetWorld()->GetDeltaSeconds();  //월드에서 시간 가져오고 fps
-			float InterpSpeed = 17.5f;  // 값이 높을수록 빠르게 회전
-
-			FRotator NewRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, DeltaTime, InterpSpeed);
-			SetActorRotation(NewRotation);
-
-			
-
-			// 게임 화면에 출력 (Debug)
-			if (GEngine)
-			{
-				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("회전!"));
-			}
-		}
-		//n초짜리 타이머 생성
-		Bhashit = true;
-		GetWorld()->GetTimerManager().SetTimer(RotationCooldownTimer, this, &AEnemyMovingOutCharacter::ResetRotationCooldown, 0.2f, false);
-	
+		return;
 	}
 
+	if (OtherActor && OtherActor != this)
+	{
+		// 충돌한 지점과 나의 위치로부터 충돌 방향 벡터 계산
+		FVector ImpactDirection = Hit.ImpactPoint - GetActorLocation();
+		ImpactDirection.Normalize();
+
+		//내 현재 앞방향 벡터
+		FVector ForwardVector = GetActorForwardVector();
+
+		//외적값을 계산
+		FVector CrossProduct = FVector::CrossProduct(ForwardVector, ImpactDirection);
+
+		//내 회전 값 가져오기
+		FRotator NewRotation = GetActorRotation();
+
+		//  외적의 Z값을 보고 어느 방향으로 부딪혔는지 판별
+		FRotator CurrentRotation = GetActorRotation();
+		if (CrossProduct.Z > 0)
+		{
+			// 오른쪽에 부딪혔으니 왼쪽으로 튼다
+			TargetRotation = CurrentRotation.Add(0.0f, -75.0f, 0.0f);
+		}
+		else
+		{
+			// 왼쪽에 부딪혔으니 오른쪽으로 튼다
+			TargetRotation = CurrentRotation.Add(0.0f, 75.0f, 0.0f);
+		}
+
+		// 회전 시작 상태로 설정
+		bIsRotating = true;
+
+		// 화면 출력 (디버그)
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("회전!"));
+		}
+
+		// 쿨타임 시작 쿨타임은 n.f 값만큼 넣음
+		Bhashit = true;
+		GetWorld()->GetTimerManager().SetTimer(RotationCooldownTimer, this, &AEnemyMovingOutCharacter::ResetRotationCooldown, 1.0f, false);
+		
+		
+	}
+	
+}
 // 다시 처음으로 돌아가게
 void AEnemyMovingOutCharacter::ResetRotationCooldown()
 {
 	Bhashit = false;
 }
+// 위에까지 OnHit event
+
+
 // 애로우 컴포넌트 추가 밑에서 변수 넣으면 활용가능
 AEnemyMovingOutCharacter::AEnemyMovingOutCharacter()
 {
@@ -105,9 +110,30 @@ AEnemyMovingOutCharacter::AEnemyMovingOutCharacter()
 void AEnemyMovingOutCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	if (bIsRotating)
+	{
+		FRotator CurrentRotation = GetActorRotation();
+		float InterpSpeed = 7.0f; 
+
+		FRotator InterpolatedRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, DeltaSeconds, InterpSpeed);
+		SetActorRotation(InterpolatedRotation);
+
+		// 목표 회전에 거의 도달했는지 확인
+		
+		if (FMath::IsNearlyEqual(CurrentRotation.Yaw, TargetRotation.Yaw, 1.0f))
+		{
+			bIsRotating = false; // 회전이 끝나면 플래그를 false로
+		}
+		
+	}
 	// 틱마다 fVector x 축으로 쭉 간다
 	FVector Direction = GetActorForwardVector();
 	AddMovementInput(Direction, 1.0f);
 	
-	
-}
+	// 라인 트레이서 DrawDebugLineTrace(GetWorld(),start ,end,)
+	// 트레이서  히트 포인트 DrawDebugHitPoint(GetWorld(),FHitResult,)
+}	
+
+
+
--- Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
+++ Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
@@ -15,18 +15,39 @@ class MOVINGOUT_API AEnemyMovingOutCharacter : public AMovingOutCharacter
 	GENERATED_BODY()
 
 
-	// 틱을 오버드라이브 함
+	// 틱을 오버라이브 함
 public:
 	AEnemyMovingOutCharacter();
 	void Tick(float DeltaSeconds) override;
+	//타겟을둠
+	
+	
+	// 회전에 대한 float 변수들
+	UPROPERTY(EditAnywhere)
+	float RotationCurrentSpeed = 10.0f;
 
-	// 비긴플레이 를 오버드라이브 일부만 하용하게
+	UPROPERTY(EditAnywhere)
+	float RotationMaxSpeed = 50.f;
+
+	UPROPERTY(EditAnywhere)
+	float RotationAcceleration = 5.0f;
+
+	UPROPERTY(EditAnywhere)
+	FRotator ETargetRotaion;
+
+
+	
+	// 비긴플레이 를 오버라이브 일부만 하용하게
 protected:
 	virtual void BeginPlay() override;
 	
 private:
+	// 나의 회전 위치를 지정하기 위해
+	FRotator TargetRotation;
 	// 닿았을때 false 인 boolean
 	bool Bhashit = false;
+	// 돌았을떄 false 인 boolean
+	bool bIsRotating;
 
 	// 헤더에서 타이머 생성
 	FTimerHandle RotationCooldownTimer;
@@ -34,8 +55,8 @@ private:
 
 	// 에너미에 힛 포인트를 불러온다
 	UFUNCTION()
-	void OnEnemyHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
-	
+	void  OnEnemyHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 	// arrow 불러오기
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UArrowComponent* Arrow;
