d7190329d5afbcce954fb13a97ffb56cd961044a
jammim0209 <woals1375@naver.com>
2025-08-08 16:48:24 +0900

    player Targeting 및 Tracking 기능 구현

--- Source/MovingOut/Private/Character/EnemyMovingOutCharacter.cpp
+++ Source/MovingOut/Private/Character/EnemyMovingOutCharacter.cpp
@@ -1,44 +1,49 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Character/EnemyMovingOutCharacter.h"
-#include "Components/ArrowComponent.h"
+ // #include "Components/ArrowComponent.h"
 #include "Character/MovingOutCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Components/CapsuleComponent.h"
-#include "GameFramework/CharacterMovementComponent.h"
+ // #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/PlayerController.h"
 #include "Materials/Material.h"
 #include "Engine/World.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "../DebugHelpers.h"
-
+#include "Kismet/GameplayStatics.h"
+#include  "NavigationSystem.h"
+ // #include "../DebugHelpers.h"
  
+
+
 
 // 캡술컴포넌트에 닿았을때 인지 없다면 에러뜨고 없다고 뜨기
 void AEnemyMovingOutCharacter::BeginPlay()
 {
+	
 	Super::BeginPlay();
+
+	UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+	
+	PlayerMovingOutCharacter = Cast<AMovingOutCharacter>(UGameplayStatics::GetPlayerCharacter(this, 0));
+	
 	if(GetCapsuleComponent())
-	{ // 캡슐 컨포넌트에 add dynamic 추가 - 온 히트 할 시에 밑에 시뮬레이션 피직스 닿을떄 리깅 닿아져 있는 내 몸에 닿을떄
-		GetCapsuleComponent()->OnComponentHit.AddDynamic(this, &AEnemyMovingOutCharacter::OnEnemyHit);
+	{
+		GetCapsuleComponent()->OnComponentHit.AddDynamic(this, &AEnemyMovingOutCharacter::OnEnemyHit); // 캡슐 콜리전 이 내몸에 닿을때 Add Dynamic
 		GetCapsuleComponent()->SetNotifyRigidBodyCollision(true);
 		GetCapsuleComponent()->SetCollisionProfileName(TEXT("BlockAllDynamic"));
-		// 존재 한다면 존재 한다는 텍스트 띄움	
-		UE_LOG(LogTemp, Log , TEXT("Enemy에 캡슐 컴포넌트 존재 확인")); 
+		UE_LOG(LogTemp, Log , TEXT("Enemy에 캡슐 컴포넌트 존재 확인"));
 	}
 	else
 	{	// 차후에 destroy 될일 있다면 없다고 뜰 예정 근데이거 tick 옮겨야 하는거 아닌가 
 		UE_LOG(LogTemp, Error, TEXT("Enemy에 캡슐 컴포넌트가 없습니다!"));
 	}
 }
-
 void AEnemyMovingOutCharacter::OnEnemyHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	// 충돌 로그 찍기
 	UE_LOG(LogTemp, Warning, TEXT("Enemy가 %s와 충돌했습니다!"), *OtherActor->GetName());
-
-	//이미 Hit 처리 중이면 리턴
+	
 	if (Bhashit)
 	{
 		return;
@@ -46,49 +51,37 @@ void AEnemyMovingOutCharacter::OnEnemyHit(UPrimitiveComponent* HitComp, AActor* 
 
 	if (OtherActor && OtherActor != this)
 	{
-		// 충돌한 지점과 나의 위치로부터 충돌 방향 벡터 계산
 		FVector ImpactDirection = Hit.ImpactPoint - GetActorLocation();
-		ImpactDirection.Normalize();
+		ImpactDirection.Normalize();  //노말라이즈 시켜
 
-		//내 현재 앞방향 벡터
 		FVector ForwardVector = GetActorForwardVector();
-
-		//외적값을 계산
 		FVector CrossProduct = FVector::CrossProduct(ForwardVector, ImpactDirection);
 
-		//내 회전 값 가져오기
-		FRotator NewRotation = GetActorRotation();
-
-		//  외적의 Z값을 보고 어느 방향으로 부딪혔는지 판별
 		FRotator CurrentRotation = GetActorRotation();
 		if (CrossProduct.Z > 0)
 		{
-			// 오른쪽에 부딪혔으니 왼쪽으로 튼다
-			TargetRotation = CurrentRotation.Add(0.0f, -75.0f, 0.0f);
+			TargetRotation = CurrentRotation.Add(0.0f, -45.0f, 0.0f); // 왼쪽으로 회전
 		}
 		else
 		{
-			// 왼쪽에 부딪혔으니 오른쪽으로 튼다
-			TargetRotation = CurrentRotation.Add(0.0f, 75.0f, 0.0f);
+			TargetRotation = CurrentRotation.Add(0.0f, 45.0f, 0.0f); // 오른쪽으로 회전
 		}
 
-		// 회전 시작 상태로 설정
+		// 회전을 시작하고 이동을 멈추도록 플래그를 설정합니다.
 		bIsRotating = true;
+		bIsMoving = false; // 충돌 즉시 앞으로 이동을 멈춥니다.
 
-		// 화면 출력 (디버그)
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("회전!"));
 		}
 
-		// 쿨타임 시작 쿨타임은 n.f 값만큼 넣음
-		Bhashit = true;
-		GetWorld()->GetTimerManager().SetTimer(RotationCooldownTimer, this, &AEnemyMovingOutCharacter::ResetRotationCooldown, 1.0f, false);
-		
-		
+		Bhashit = true;   // 타이머는 추후 수정 예정
+		GetWorld()->GetTimerManager().SetTimer(RotationCooldownTimer, this, &AEnemyMovingOutCharacter::ResetRotationCooldown, 0.65f, false);
 	}
-	
 }
+
+
 // 다시 처음으로 돌아가게
 void AEnemyMovingOutCharacter::ResetRotationCooldown()
 {
@@ -97,43 +90,75 @@ void AEnemyMovingOutCharacter::ResetRotationCooldown()
 // 위에까지 OnHit event
 
 
-// 애로우 컴포넌트 추가 밑에서 변수 넣으면 활용가능
-AEnemyMovingOutCharacter::AEnemyMovingOutCharacter()
-{
-	PrimaryActorTick.bCanEverTick = true;
-	if(Arrow)
-	{
-		Arrow->SetupAttachment(RootComponent);
-	}
-}
+
 // 델타 타임 줄떄마다 계속 활동
 void AEnemyMovingOutCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
+	
+	// 회전 중일 때
 	if (bIsRotating)
 	{
+		// RInterpTo 함수를 사용하여 현재 회전 값에서 목표 회전 값으로 부드럽게
+		// InterpSpeed를 조절하여 회전 속도를 변경
 		FRotator CurrentRotation = GetActorRotation();
-		float InterpSpeed = 7.0f; 
+		float InterpSpeed = 8.0f; 
 
 		FRotator InterpolatedRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, DeltaSeconds, InterpSpeed);
 		SetActorRotation(InterpolatedRotation);
 
-		// 목표 회전에 거의 도달했는지 확인
-		
+		// 목표 회전 값에 거의 도달했는지 확인
 		if (FMath::IsNearlyEqual(CurrentRotation.Yaw, TargetRotation.Yaw, 1.0f))
 		{
-			bIsRotating = false; // 회전이 끝나면 플래그를 false로
+			bIsRotating = false; // 회전이 끝나면 회전 플래그를 false로
+			bIsMoving = true; // 앞으로 다시 이동 시작
 		}
 		
+		// 회전 중일 때만 이동
+		if (bIsMoving)
+		{
+			return;	
+		};
+		FVector Direction = GetActorForwardVector();
+		// 캐릭터를 전방 방향으로 이동
+		AddMovementInput(Direction, 1.0f);
+		bIsRotating = true;
+		bIsMoving = false;
+
 	}
-	// 틱마다 fVector x 축으로 쭉 간다
-	FVector Direction = GetActorForwardVector();
-	AddMovementInput(Direction, 1.0f);
+	else if (PlayerMovingOutCharacter)  //PlayerMovingChatacter 추적
+	{
+		//playerMovingOutCharacter 의 lcoation 값을 가져옴
+		FVector PlayerLocation = PlayerMovingOutCharacter->GetActorLocation();
+		FVector EnemyLocation = GetActorLocation();
+
+		// 플레이어를 향하는 방향 벡터를 계산 + normalize
+		FVector DirectionToPlayer = PlayerLocation - EnemyLocation;
+		DirectionToPlayer.Normalize();
+
+		// 방향 벡터를 FRotator로 변환하여 목표 회전 값으로 사용
+		FRotator TargetPlayerRotation = DirectionToPlayer.Rotation();
+    
+		FRotator CurrentRotation = GetActorRotation();
+		float InterpSpeed = 10.0f; // 회전 속도
+
+		// 현재 회전을 목표 회전 값으로 부드럽게 보간
+		FRotator InterpolatedRotation = FMath::RInterpTo(CurrentRotation, TargetPlayerRotation, DeltaSeconds, InterpSpeed);
+		SetActorRotation(InterpolatedRotation);
+
+		// 보간된 회전 방향으로 이동하는 것이 아닌,
+		// 처음 계산한 방향 벡터로 이동
+		AddMovementInput(DirectionToPlayer, 1.0f);
+	}
+	// 플레이어가 없거나 회전 중이 아닐 때 기존 이동 로직
+	else
+	{
+		FVector Direction = GetActorForwardVector();
+		AddMovementInput(Direction, 1.0f);
+	}
+
+
 	
-	// 라인 트레이서 DrawDebugLineTrace(GetWorld(),start ,end,)
-	// 트레이서  히트 포인트 DrawDebugHitPoint(GetWorld(),FHitResult,)
-}	
-
-
+};		
 
--- Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
+++ Source/MovingOut/Public/Character/EnemyMovingOutCharacter.h
@@ -17,39 +17,30 @@ class MOVINGOUT_API AEnemyMovingOutCharacter : public AMovingOutCharacter
 
 	// 틱을 오버라이브 함
 public:
-	AEnemyMovingOutCharacter();
 	void Tick(float DeltaSeconds) override;
-	//타겟을둠
-	
-	
-	// 회전에 대한 float 변수들
-	UPROPERTY(EditAnywhere)
-	float RotationCurrentSpeed = 10.0f;
-
-	UPROPERTY(EditAnywhere)
-	float RotationMaxSpeed = 50.f;
-
-	UPROPERTY(EditAnywhere)
-	float RotationAcceleration = 5.0f;
-
-	UPROPERTY(EditAnywhere)
-	FRotator ETargetRotaion;
-
-
 	
 	// 비긴플레이 를 오버라이브 일부만 하용하게
 protected:
 	virtual void BeginPlay() override;
 	
 private:
+	//플레이어 캐릭터 타겟팅
+	AMovingOutCharacter* PlayerMovingOutCharacter;
+	
 	// 나의 회전 위치를 지정하기 위해
 	FRotator TargetRotation;
 	// 닿았을때 false 인 boolean
 	bool Bhashit = false;
 	// 돌았을떄 false 인 boolean
 	bool bIsRotating;
-
-	// 헤더에서 타이머 생성
+	// 움직였을떄 false 인 boolean
+	bool bIsMoving;
+	
+	// Forward 타이머 핸들
+	FTimerHandle MovementTimerHandle;
+	void EnemyMoveForward();
+	
+	// Rotation 타이머 핸들
 	FTimerHandle RotationCooldownTimer;
 	void ResetRotationCooldown();
 
